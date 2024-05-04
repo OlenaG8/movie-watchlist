0f76f8bc17fb69983e1b7d5157fef78f3b41e834
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-05-04 15:01:23 +0200

    Working on dark theme

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
-const toggleDarkMode = document.getElementById("toggle-dark-mode")
+const themeToggle = document.getElementById("theme-toggle")
 const body = document.querySelector("body")
 
-toggleDarkMode.addEventListener("click", function(){
-    this.classList.toggle("fa-moon")
-    if (this.classList.toggle("fa-sun")) {
-        body.style.background = '#efefef'
-        body.style.color = '#121212'
-        body.style.transition = '2s'
-    } else {
-        body.style.background = '#121212'
-        body.style.color = '#efefef'
-        body.style.transition = '2s'
-    }
-})
+// document.addEventListener("DOMContentLoaded", function(){
+//     themeToggle.classList.toggle("fa-moon")
+
+//     if (themeStylesheet.href.includes('light')) {
+//         themeStylesheet.href = 'dark-theme.css'
+//         themeToggle.innerText = 'Switch to light mode'
+//       } else {
+//         themeStylesheet.href = 'light-theme.css'
+//         themeToggle.innerText = 'Switch to dark mode'
+//       }
+
+// })
