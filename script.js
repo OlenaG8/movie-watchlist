931bce4b7f5748b29a4ce8cd0720b1f6291a2b38
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-05-06 23:29:36 +0200

    Working on the dark theme

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ themeToggle.addEventListener("click", function(){
     this.classList.toggle("fa-moon")
     if (this.classList.toggle("fa-sun")) {
         document.documentElement.setAttribute('data-theme', 'light');
+        document.getElementById("explore-icon").setAttribute('src', 'assets/explore-icon.png')
         body.style.transition = '2s'
     } else {
         document.documentElement.setAttribute('data-theme', 'dark');
+        document.getElementById("explore-icon").setAttribute('src', 'assets/explore-icon-dark.png')
         body.style.transition = '2s'
     }
 })
