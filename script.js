d6c05e42b1184205809dc2128f0aa1c1d49ca8f5
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-05-06 23:07:28 +0200

    Working on dark theme

--- script.js
+++ script.js
@@ -5,10 +5,10 @@ const themeToggle = document.getElementById('theme-toggle');
 themeToggle.addEventListener("click", function(){
     this.classList.toggle("fa-moon")
     if (this.classList.toggle("fa-sun")) {
-        document.documentElement.setAttribute('data-theme', 'dark');
+        document.documentElement.setAttribute('data-theme', 'light');
         body.style.transition = '2s'
     } else {
-        document.documentElement.setAttribute('data-theme', 'light');
+        document.documentElement.setAttribute('data-theme', 'dark');
         body.style.transition = '2s'
     }
 })
