a5a4fbfd42a213782186238e4d314d392b8906bd
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-05-16 22:22:43 +0200

    Added movie rendering on the main page

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const searchBtn = document.getElementById('search-btn')
 const userInput = document.getElementById('search-movie')
 
+const foundMovies = document.getElementById('found-movies')
+
 let moviesListArr = []
 
 const themeToggle = document.getElementById('theme-toggle');
@@ -21,28 +23,69 @@ themeToggle.addEventListener("click", function(){
     }
 })
 
-const options = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI5MjRhNjc3MWNjNTYyZGViYjZkMzI0NjFlNmI0M2Y2NCIsInN1YiI6IjY2NDIyY2I0MzQwZTlhOWE3ZTM3NmRiYyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0aOlrqmyK-AOH8_Ywx-RP0XIjPVMLB0mZG6unYOx2qA'
-    }
-}
 
-function getTheSearchResult() {
+// searchBtn.addEventListener('click', getSearchResults)
+
+// function getSearchResults() {
     
-    fetch(`https://api.themoviedb.org/3/search/movie?query=${userInput.value}&include_adult=false&language=en-US&page=1`, options)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.results)
-        })
-        .catch(err => console.error(err))
+//     fetch(`https://api.themoviedb.org/3/search/movie?query=${userInput.value}&include_adult=false&language=en-US&page=1`, options)
+//     .then(res => res.json())
+//     .then(data => {
+
+//         const movieID = data.results[0].id
+
+//         console.log(movieID)
+//         })
+//         .catch(err => console.error(err))
+// }
+
+const apiKey =  `c83e1e03`
+
+searchBtn.addEventListener('click', async (e) => {
+    e.preventDefault()
+    await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${userInput.value}`)
+        .then(res => res.json())
+        .then(data => getSearchDetails(data.Search))
+})
+
+async function getSearchDetails(movies) {
+    moviesListArr = []
+    for(let movie of movies) {
+        await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                moviesListArr.push(data)
+            })
+    }
+    render()
 }
-    
-searchBtn.addEventListener('click', getTheSearchResult)
+ 
 
 function getMovieHtml(movie) {
     return `
-    <div>
+    <div class="movie-cont">
+        <img src="${movie.Poster}" class="movie-poster" alt="${movie.Title}">
+        <div class="mov-headline">
+            <h2>${movie.Title}</h2>
+            <h3>⭐${movie.imdbRating}</h3>
+        </div>
+        <div class="mov-details">
+            <h4>${movie.Genre}</h4>
+            <h4>${movie.Runtime}</h4>
+            <div class="watchlist-btn">
+                <i class="fa-solid fa-circle-plus"></i>
+                <h4>Watchlist</h4>
+            </div>
+        </div>
+        <p>${movie.Plot}</p>
+     </div>
     `
+}
+
+function render() {
+    let html = ""
+    html += moviesListArr.map(getMovieHtml).join('')
+    document.getElementById('start-page').style.display = 'none'
+    foundMovies.innerHTML = html
 }
