1009d1c9dee11818c2c9743a268796c04119224f
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-05-18 15:02:25 +0200

    Improved runtime

--- script.js
+++ script.js
@@ -1,11 +1,26 @@
+const apiKey =  `c83e1e03`
+
 const searchBtn = document.getElementById('search-btn')
 const userInput = document.getElementById('search-movie')
 
+const startPageCon = document.getElementById('start-page')
+
 const foundMovies = document.getElementById('found-movies')
 
-let moviesListArr = []
+const myWatchlistCon = document.getElementById('my-watchlist-con')
 
-const themeToggle = document.getElementById('theme-toggle');
+let moviesListArr = []
+let myWatchlist = []
+
+const themeToggle = document.getElementById('theme-toggle')
+const errMessage = `Unable to find what you're looking for. Please try again`
+
+document.addEventListener("click", (e) => {
+    if (e.target.dataset.movieid) {
+        console.log(e.target.dataset.movieid)
+        // addToMyWatchlist(e.target.dataset.movieid)
+     }
+}) 
 
 themeToggle.addEventListener("click", function(){
     const body = document.querySelector("body")
@@ -23,45 +38,38 @@ themeToggle.addEventListener("click", function(){
     }
 })
 
-
-// searchBtn.addEventListener('click', getSearchResults)
-
-// function getSearchResults() {
-    
-//     fetch(`https://api.themoviedb.org/3/search/movie?query=${userInput.value}&include_adult=false&language=en-US&page=1`, options)
-//     .then(res => res.json())
-//     .then(data => {
-
-//         const movieID = data.results[0].id
-
-//         console.log(movieID)
-//         })
-//         .catch(err => console.error(err))
-// }
-
-const apiKey =  `c83e1e03`
-
 searchBtn.addEventListener('click', async (e) => {
+    startPageCon.style.display = 'flex'
+    foundMovies.innerHTML = ''
     e.preventDefault()
+
     await fetch(`https://www.omdbapi.com/?i=tt3896198&apikey=${apiKey}&s=${userInput.value}`)
         .then(res => res.json())
         .then(data => getSearchDetails(data.Search))
+        .catch(err => {
+            console.log(err)
+            startPageCon.innerHTML = `<p>${errMessage}</p>`
+        })
 })
 
 async function getSearchDetails(movies) {
     moviesListArr = []
-    for(let movie of movies) {
-        await fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
-            .then(res => res.json())
-            .then(data => {
-                console.log(data)
-                moviesListArr.push(data)
-            })
+
+    try {
+        const moviePromises = movies.map(movie =>
+            fetch(`https://www.omdbapi.com/?apikey=${apiKey}&i=${movie.imdbID}`)
+                .then(res => res.json())
+        )
+
+        const moviesListArr = await Promise.all(moviePromises)
+        render(moviesListArr)
+
+    } catch (err) {
+        console.log('Error fetching movie details:', err)
+        startPageCon.innerHTML = `<p>${errMessage}</p>`
     }
-    render()
 }
  
-
 function getMovieHtml(movie) {
     return `
     <div class="movie-cont">
@@ -74,8 +82,8 @@ function getMovieHtml(movie) {
             <h4>${movie.Genre}</h4>
             <h4>${movie.Runtime}</h4>
             <div class="watchlist-btn">
-                <i class="fa-solid fa-circle-plus"></i>
-                <h4>Watchlist</h4>
+                <i class="fa-solid fa-circle-plus" data-movieid=${movie.imdbID}></i>
+                <h4 data-movieid=${movie.imdbID}>Watchlist</h4>
             </div>
         </div>
         <p>${movie.Plot}</p>
@@ -83,9 +91,43 @@ function getMovieHtml(movie) {
     `
 }
 
-function render() {
+function getMyWatchlistHtml() {
+    return `
+    <div class="movie-cont">
+        <img src="${movie.Poster}" class="movie-poster" alt="${movie.Title}">
+        <div class="mov-headline">
+            <h2>${movie.Title}</h2>
+            <h3>⭐${movie.imdbRating}</h3>
+        </div>
+        <div class="mov-details">
+            <h4>${movie.Genre}</h4>
+            <h4>${movie.Runtime}</h4>
+            <div class="watchlist-btn">
+                <i class="fa-solid fa-circle-minus"></i>
+                <h4>Remove</h4>
+            </div>
+        </div>
+        <p>${movie.Plot}</p>
+     </div>
+    `
+}
+
+function render(movieArr) {
     let html = ""
-    html += moviesListArr.map(getMovieHtml).join('')
-    document.getElementById('start-page').style.display = 'none'
+    html += movieArr.map(getMovieHtml).join('')
+    startPageCon.style.display = 'none'
     foundMovies.innerHTML = html
+}
+
+function renderWatchlist() {
+    let html = "";
+    html += myWatchlistArr.map(getMyWatchlistHtml).join('');
+    myWatchlistCon.innerHTML = html;
+}
+
+function addToWatchlist(id) {
+    
+    myWatchlist.push(movie)
+    updateLocalStorage()
+    renderWatchlist()
 }
